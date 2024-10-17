a141ae735b786b8a34419f390ea92621c676034b
leanghok120 <leanghokoeng5@gmail.com>
2024-10-17 20:03:14 +0700

    Setup page routing for frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,34 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Create from "./pages/Create";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/create",
+    element: <Create />,
+  },
+]);
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
