db86fc192642f6c5930a564e0b293a520d30eb2b
leanghok120 <leanghokoeng5@gmail.com>
2024-10-18 04:44:25 +0700

    Add header and footer

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Create() {
-  return <div>Create</div>;
+  return (
+    <div>
+      <Header />
+      <h1>Create</h1>
+      <Footer />
+    </div>
+  );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+      <h1>Home</h1>
+      <Footer />
+    </div>
+  );
 }
