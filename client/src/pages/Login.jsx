da842c79263fe585176764bcd7a93adc0ad9cebd
leanghok120 <leanghokoeng5@gmail.com>
2024-10-18 19:39:30 +0700

    Setup daisyui and Style register and login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,27 @@
 import React from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="h-screen flex flex-col justify-center items-center">
+      <h1 className="font-bold text-3xl mb-5">Login</h1>
+      <form
+        className="flex flex-col gap-2"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Username"
+          className="input input-bordered"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="input input-bordered"
+        />
+        <button class="btn btn-primary">Login</button>
+      </form>
+    </div>
+  );
 }
