d5f743bcf8024184ae88c4486b52afde93a2a5a7
leanghok120 <leanghokoeng5@gmail.com>
2024-10-18 19:59:53 +0700

    Style header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="navbar bg-base-100">
+      <div className="flex-1">
+        <Link className="btn btn-ghost text-2xl font-bold" to={"/"}>
+          Boba Bliss
+        </Link>
+      </div>
+      <ul className="menu menu-horizontal px-1">
+        <li>
+          <Link className="link-hover font-bold" to={"/create"}>
+            Create
+          </Link>
+        </li>
+        <li>
+          <Link className="link-hover font-bold" to={"/register"}>
+            Register
+          </Link>
+        </li>
+        <li>
+          <Link className="link-hover font-bold" to={"/login"}>
+            Login
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
 }
