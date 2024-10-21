40556785f3eea4488c3eaa5866a333f5dfab2994
leanghok120 <leanghokoeng5@gmail.com>
2024-10-21 19:57:30 +0700

    Style Create page

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -7,7 +7,21 @@ export default function Create() {
     <div className="w-2/5 h-screen mx-auto bg-base-300 p-2">
       <Header />
       <div className="mt-12 px-8">
-        <h1 className="font-black text-4xl">Create</h1>
+        <h1 className="font-black text-4xl">Add Something To Drink</h1>
+        <h2 className="font-bold text-2xl mt-6">What's your favorite drink?</h2>
+        <form
+          className="flex flex-col gap-4 mt-6"
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Your drink"
+            className="input input-bordered"
+          />
+          <button class="btn btn-primary text-2xl rounded-full">Create</button>
+        </form>
       </div>
       <Footer />
     </div>
