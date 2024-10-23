95152c4164b353b88867abe79aa20b02c06ad3b3
leanghok120 <leanghokoeng5@gmail.com>
2024-10-23 19:24:28 +0700

    Add routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import express from "express";
+import drinksRoute from "./routes/drinks.js";
 
 const app = express();
+
+app.use(express.json());
+app.use("/api/drinks", drinksRoute);
 
 app.get("/", (req, res) => {
   res.json("This is the server :3");
@@ -9,3 +13,4 @@ app.get("/", (req, res) => {
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
 });
+
